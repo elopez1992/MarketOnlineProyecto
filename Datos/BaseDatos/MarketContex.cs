9dde7974b2dece7256e3b6a29e1a418dad08f850
elopez1992 <Edlopez2054@Gmail.com>
2024-03-16 19:34:43 -0600

    Agregar archivos de proyecto.

---
+++ Datos/BaseDatos/MarketContex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.BaseDatos
+{
+    public class MarketContex: DbContext
+    {
+        public MarketContex():base("Name=MarketOnline")
+        {
+
+        }
+    }
+
+}
